8d4f245026d51a550c5de570dcb1c1d58dd98295
Roman Mainer <32781692+m0nolith@users.noreply.github.com>
2019-04-15 22:06:42 +0200

    Create run-as-admin.ps1
    PowerShell admin check

---
+++ powershell/run-as-admin.ps1
@@ -0,0 +1,21 @@
+#Requires -RunAsAdministrator
+
+function Test-Administrator  
+{  
+    [OutputType([bool])]
+    param()
+    process {
+        [Security.Principal.WindowsPrincipal]$user = [Security.Principal.WindowsIdentity]::GetCurrent();
+        return $user.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator);
+    }
+}
+
+if(-not (Test-Administrator))
+{
+    Write-Error "This script must be executed as Administrator.";
+    exit 1;
+}
+
+$ErrorActionPreference = "Stop";
+
+# do something
