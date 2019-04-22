199df226af6f401adcd72750db5570e465d1d08f
Roman Mainer <32781692+m0nolith@users.noreply.github.com>
2019-04-22 18:39:38 +0200

    Added levenshtein distance implementations

---
+++ algorithms/levenstein-distance/Levenshtein.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Levenshtein
+{
+    class Levenshtein
+    {
+        private static int LevenshteinDistance(String w1, String w2)
+        {
+            w1 = w1.ToLower();
+            w2 = w2.ToLower();
+            int n1 = w1.Length + 1;
+            int n2 = w2.Length + 1;
+            int[,] d = new int[n1, n2];
+            Array.Clear(d, 0, n1 * n2);
+
+            for (int y = 0; y < n1; y++) d[y, 0] = y;
+            for (int x = 0; x < n1; x++) d[0, x] = x;
+
+            for (int y = 1; y < n1; y++)
+            {
+                for (int x = 1; x < n2; x++)
+                {
+                    if (w1[y - 1] == w2[x - 1])
+                    {
+                        d[y, x] = d[y - 1, x - 1];
+                    }
+                    else
+                    {
+                        d[y, x] = Math.Min(d[y - 1, x - 1], Math.Min(d[y - 1, x], d[y, x - 1])) + 1;
+                    }
+                }
+            }
+
+            return d[n1 - 1, n2 - 1];
+        }
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(LevenshteinDistance("Roman", "Ramon"));
+        }
+    }
+}
---
+++ algorithms/levenstein-distance/README.md
@@ -0,0 +1,9 @@
+# Levenshtein distance
+
+Implementations for the levenshtein distance in serveral programming languages.
+
+* C#: `csc /t:exe /out:Levensthein.exe Levenshtein.cs` (C:\Windows\Microsoft.NET\Framework64\v4.0.30319)
+* Java: `java levenstein.java`
+* JavaScript: `node levenstein.js`
+* PHP: `php levenstein.php`
+* Python: `python levenstein.py`
